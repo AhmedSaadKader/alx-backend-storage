33369ef7784a317559d7e1ac72f891114efb815c
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-08-14 16:10:53 +0300

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,4 +16,20 @@ class Cache():
       key = str(uuid4())
       self._redis.set(key, data)
       return key
-  
+
+  def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+      """Get data from cache"""
+      if fn:
+          return fn(self._redis.get(key))
+      return self._redis.get(key)
+
+  def get_str(self, key: str) -> str:
+      return self._redis.get(key).decode('utf-8')
+
+  def get_int(self, key:str) -> int:
+      value = self._redis.get(key)
+      try:
+          value = int(value.decode('utf-8'))
+      except Exception:
+          value = 0
+      return value
