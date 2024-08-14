854a07e57070d4a7deebbcb248d91d798cfb4d63
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-08-14 15:54:31 +0300

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 """Writing strings to Redis"""
 import redis
+from uuid import uuid4
 
 
 class Cache():
   """Cach class"""
 
   def __init__(self):
-    self._redis = redis.Redis()
-    self._redis.flushdb()
+      self._redis = redis.Redis()
+      self._redis.flushdb()
+
+  def store(data):
+      key = str(uuid4())
+      self._redis.set(key, data)
